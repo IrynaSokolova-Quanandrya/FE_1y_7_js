1d6d5a1cf789b8e690d57640a4c3e2f155bc6bff
Iryna <irasokolova25@ukr.net>
2022-09-20 20:46:09 +0300

    додала код уроку

---
+++ index-1.js
@@ -0,0 +1,15 @@
+
+const a = 'Я значення змінної "a" в index-1';
+// console.log(a.length);
+console.log(a);
+// console.log(a.toLocaleUpperCase());
+const b = 10;
+ console.log(b);
+
+//  const a = 'Я значення змінної "a" в index-2';
+
+// console.log(a);
+
+// const f = 5;
+
+//  console.log(f);
---
+++ index-2.js
@@ -0,0 +1,33 @@
+
+const a = 'Я значення змінної "a" в index-2';
+
+// console.log(a);
+
+const f = 5;
+
+//  console.log(f);
+
+const firstName = "Ira";
+console.log(typeof firstName);
+
+const isOnline = true; 
+console.log(typeof isOnline);
+
+const age = 33.3;
+console.log(typeof age);
+
+const withoutInit = null;
+console.log(typeof  withoutInit);
+
+
+// let b;
+// console.log(b);
+// b = "hello!";
+// console.log("Змінна b:", b);
+// b = "buy!"
+// console.log(b);
+
+// const c = 2;
+// c = 3;
+
+//  console.log("Hi!");
