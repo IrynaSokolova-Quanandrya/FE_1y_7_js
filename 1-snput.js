4d37d9d72bfa0cb4d762aaa013d1520935cbbe50
Iryna <irasokolova25@ukr.net>
2022-09-22 17:09:42 +0300

    Create 1-snput.js

---
+++ 1-snput.js
@@ -0,0 +1,12 @@
+/*
+ * - Ввод користувача
+ * - Методи window.confirm() и window.prompt()
+ */
+
+// const shouldRenew = confirm('Бажаєте продовжити підписку ?');
+// console.log(shouldRenew);
+
+// let quantity = prompt('Введіть кількість товарів');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
