bda8336f720c70c0305d91d4e33068322dfc2fd8
Iryna <irasokolova25@ukr.net>
2022-09-27 16:15:28 +0300

    add plan

---
+++ 2-number.js
@@ -0,0 +1,51 @@
+/*
+ * Парс числа з Number.parseInt() і Number.parseFloat()
+ */
+
+let elementWidth = '50px';
+// elementWidth = ;
+// console.log('elementWidth: ', elementWidth);
+
+let elementHeight = '200.74px';
+// elementHeight = ;
+// console.log('elementHeight: ', elementHeight);
+
+/*
+ * - Метод число.toFixed(digits)
+ * - Матрьошка на прикладі  console.log(Number(число.toFixed(digits)))
+ */
+const salary = 1300.16472;
+
+/*
+ * - Приведення до числа з Number(value)
+ * - Значення NaN (Not a Number) і метод Number.isNaN(value)
+ */
+let quantity = '30';
+let value = 'Цей рядок неможливо привести до числа';
+
+
+
+/*
+ * Обьект Math
+ * - Підведення до ступеня
+ * - Exponent operator
+ */
+
+const base = 2;
+const power = 5;
+
+
+
+/*
+ * Напиши скрипт який просить у користувача ввести число і степінь,
+ * підводить число в цю степіньі виводить результат в консоль.
+ */
+// Алгоритм рішення:
+
+/*
+ * Генерим псевдовипадкові числа
+ * - Math.random()
+ * - Math.round()
+ */
+// Math.random() * (max - min) + min
+
