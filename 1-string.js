743da2f2a24479d476bd57b4f6565783a1b6d11e
Iryna <irasokolova25@ukr.net>
2022-09-29 12:38:47 +0300

    done

--- 1-string.js
+++ 1-string.js
@@ -3,23 +3,26 @@
  */
 const message = 'В цьому рядку 26 символів.';
 
+// console.log(message.length);
+
 /*
  * Конкатенація рядків
  */
-// const firstName = ;
-// const lastName = ;
+const firstName = "Ira";
+const lastName = "Sokolova";
 
-
+// console.log(firstName + " " + lastName);
 /*
  * Напиши скрипт який виведе рядок в форматі:
- * «Гість x y поселяється в z номер q»,
- * підставив замість x y z q значення змінних
+ * «Гість x y поселяється в g номер q»,
+ * підставив замість x y g q значення змінних
  */
 
 const room = 716;
 const type = 'VIP';
 
-
+const customer = `Гість ${firstName} ${lastName} поселяється в ${room} номер ${type}`;
+// console.log(customer);
 /*
  * Шаблонні рядки (template strings)
  * Повертаємось до складання рядка поселення в готель
@@ -33,7 +36,7 @@ const orderMsg = `Ви замовляєте 15 тістечок.`;
 /*
  * Нормалізація з методом toLowerCase()
  */
-
+// console.log();
 // Що шукаємо?
 
 /*
@@ -43,5 +46,17 @@ const blacklistedWord1 = 'спам';
 const blacklistedWord2 = 'розпродаж';
 
 const string1 = 'Привіт, я принц Абдул, це не спам, пропоную тобі мільйон!';
+// console.log(string1.includes(blacklistedWord1));
+// console.log(string1.includes(blacklistedWord2));
+
 const string2 = 'Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!';
+// console.log(string2.includes(blacklistedWord1));
+const normalizeString2 = string2.toLocaleLowerCase().includes(blacklistedWord2);
+
+console.log(normalizeString2);
+
+// console.log(normalizeString2.includes(blacklistedWord2));
+
 const string3 = 'Рекламна кампанія #jsIsAwesome';
+// console.log(string3.includes(blacklistedWord1));
+// console.log(string3.includes(blacklistedWord2));
--- 2-number.js
+++ 2-number.js
@@ -1,13 +1,24 @@
+
+// let num = '10';
+// let str = "Hello world";
+// const a = Number(str);
+// console.log(Number.isNaN(a));
+
+// let age = prompt("Скільки тобі років?")
+// console.log(age);
+// age = Number(age);
+// console.log(age);
 /*
  * Парс числа з Number.parseInt() і Number.parseFloat()
  */
 
 let elementWidth = '50px';
-// elementWidth = ;
+elementWidth = Number.parseInt(elementWidth);
+// console.log(elementWidth);
 // console.log('elementWidth: ', elementWidth);
 
-let elementHeight = '200.74px';
-// elementHeight = ;
+let elementHeight = "200.74px";
+elementHeight = Number.parseFloat(elementHeight);
 // console.log('elementHeight: ', elementHeight);
 
 /*
@@ -15,6 +26,9 @@ let elementHeight = '200.74px';
  * - Матрьошка на прикладі  console.log(Number(число.toFixed(digits)))
  */
 const salary = 1300.16472;
+const digit = 2;
+// const salaryFix = salary.toFixed(digit)
+// console.log(Number(salary.toFixed(digit)))
 
 /*
  * - Приведення до числа з Number(value)
@@ -32,20 +46,35 @@ let value = 'Цей рядок неможливо привести до числ
  */
 
 const base = 2;
-const power = 5;
+// const power = 5;
 
-
+// console.log(Math.pow(base, power));
 
 /*
  * Напиши скрипт який просить у користувача ввести число і степінь,
- * підводить число в цю степіньі виводить результат в консоль.
+ * підводить число в цю степінь і виводить результат в консоль.
  */
 // Алгоритм рішення:
+// 1. попросити в користувача число і записати його в змінну
+// const num = prompt("Дай число");
+// 2. попросити в користувача степінь і записати її в змінну
+// const power = prompt("Дай ще число");
+// 3. вивести результат підведення числа в тепінь в консоль
+// console.log(Math.pow(num, power));
 
+const res1 = 2 ** 3;
+// console.log(res1);
+// const res2 = ;
+// console.log(res2);
+// const result = res1 + res2;
+// console.log(res1 + res2);
 /*
  * Генерим псевдовипадкові числа
  * - Math.random()
  * - Math.round()
  */
-// Math.random() * (max - min) + min
+// console.log(Math.round(1.6));
 
+// console.log(Math.random() * (60 - 20) + 20);
+
+// Math.random() * (max - min) + min
