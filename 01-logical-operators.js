40f4d1dd43a7977bef7dce862e680cc6e614428f
Iryna <irasokolova25@ukr.net>
2022-10-04 20:35:03 +0300

    done

--- 01-logical-operators.js
+++ 01-logical-operators.js
@@ -10,7 +10,7 @@
  * - Повертає те на чому запнулось або останній операнд
  */
 
-// console.log(5 && 4 && 7 && 'mango');
+// console.log(1 && 4 && 7 && 'mango');
 
 /*
  * Логічне АБО (оператор ||)
@@ -18,20 +18,19 @@
  * - Повертає те на чому запнулось або останній операнд
  */
 
-// console.log(false || 5 || null || 0);
+// console.log("name" || "" || null || 0 || undefined);
 
 /*
  * Логічне НІ (оператор !)
  * Робить інверсію правда > брехня та брехня > правда
  */
 
-// console.log(!5);
+// console.log(!" ");
 
 // ПРАКТИЧНА ЗАДАЧА 1
 /*
- * Напиши скрипт який перевірить входження числа
- * у відрізок зазначений x1 та x2.
- *
+ * Напиши скрипт який перевірить входження 
+ * числа у відрізок зазначений x1 та x2. *
  * - До x1
  * - Після x2
  * - Від x1 до x2
@@ -42,18 +41,30 @@ const x1 = 10;
 const x2 = 30;
 const number = 45;
 
+
 // console.log(`Число ${number} потрапляє у відрізок до ${x1}? `, number < x1);
 
 // console.log(`Число ${number} потрапляє у відрізок після ${x2}? `, number > x2);
 
+const res1 = number > x1 && number < x2
+// number > x1 && number < x2
+// 5 > 10 && 5 < 30
+// false && true
+// false
 
-console.log(`Число ${number} потрапляє у відрізок від ${x1} до ${x2}? `, res1);
+// console.log(`Число ${number} потрапляє у відрізок від ${x1} до ${x2}? `, res1);
+
+const res2 = number < x1 || number > x2
+// number < x1 || number > x2 
+// 45 < 10 || 45 > 30
+// false || true
+// true
 
 
-console.log(
-  `Число ${number} потрапляє у відрізок до ${x1} або після ${x2}? `,
-  res2,
-);
+// console.log(
+//   `Число ${number} потрапляє у відрізок до ${x1} або після ${x2}? `,
+//   res2,
+// );
 
 // ПРАКТИЧНА ЗАДАЧА 2
 /*
@@ -64,16 +75,16 @@ console.log(
  * - без режима не турбувати
  */
 
-const isOnline = true;
+const isOnline = false;
 const isFriend = true;
-const isDnd = true;
+const isDnd = false;
 
-// const canOpenChat = ;
-// console.log('Можно открыть чат? ', canOpenChat);
+const canOpenChat = isOnline && isFriend && !isDnd;
+// console.log('Можно відкрити чат? ', canOpenChat);
 
 // ПРАКТИЧНА ЗАДАЧА 3
 /*
- * Напиши скрипт перевіркипідписки користувача під час доступу до контенту
+ * Напиши скрипт перевірки підписки користувача під час доступу до контенту
  * - Є три типи підписки: free, pro и vip.
  * - Отримати доступ можуть тільки pro и vip
  */
@@ -82,6 +93,5 @@ const sub = 'free';
 
 // якщо користувач pro або користувач vip тоді є доступ
 
-// const canAccessContent = ;
-
-// console.log('Есть доступ к контенту? ', canAccessContent);
+const canAccessContent = sub === "vip" || sub === "pro"
+// console.log('Є доступ до контенту? ', canAccessContent);
