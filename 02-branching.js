c33ae30a98a5a6eeb721c863d8a91e9586ecd9ef
Iryna <irasokolova25@ukr.net>
2022-10-04 18:54:02 +0300

    add plan

---
+++ 02-branching.js
@@ -0,0 +1,39 @@
+/*
+ * Оператор розгалуження if
+ */
+
+
+/*
+ * Оператор розгалуження if...else
+ */
+
+
+/*
+ * Оператор розгалуження else...if
+ */
+
+const salary = 5000;
+
+
+/*
+ * Тернарний оператор
+ */
+// const balance = 1000;
+// let message;
+
+// console.log(message);
+
+/*
+ * Блочная область видимости переменных
+ */
+
+// if (true) {
+//   const a = 5;
+//   console.log(b);
+// }
+
+// if (true) {
+//   const b = 10;
+
+//   console.log(a);
+// }
