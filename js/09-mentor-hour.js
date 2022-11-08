b1153d3b9bbaee63f5fae02696619a8fd5a872ed
Iryna <irasokolova25@ukr.net>
2022-11-08 17:06:31 +0200

    add file

---
+++ js/09-mentor-hour.js
@@ -0,0 +1,27 @@
+// const hello1 = function(param1, param2) {
+//     // const param1 = 2;
+//     // const param2 = 3;
+// //    const sum = param1 + param2
+// //    const sum = 2 + 3
+//     return param1 + param2;
+// };
+
+// // fn(2, 3);
+
+// const name = "Jon"
+
+// const hello2 = function(jkgjkg) {
+//     return `Привіт, ${jkgjkg}!`;
+// }
+
+// hello2(name);
+
+// console.log(hello2(name));
+
+const myNewArrowFunction = function (...args) {
+    console.log(args);
+}
+
+myNewArrowFunction(1, 2, 3);
+myNewArrowFunction(100, 200, 300, 400, 500);
+myNewArrowFunction("Js", "Python", "Java", "PHP", "C++");
