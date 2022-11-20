1da0eb5143b8b99efd62c808f339afd9647ff44e
Iryna <irasokolova25@ukr.net>
2022-11-20 13:46:23 +0200

    add iteration

--- js/01-3-iteration.js
+++ js/01-3-iteration.js
@@ -8,4 +8,33 @@ const feedback = {
     bad: 3,
   };
   
-  
+  // Number Array Boolean Object
+
+  const keys = Object.keys(feedback);
+  // console.log(keys);
+
+  // let totalFeedback = 0;
+
+  // for (const key of keys) {
+  //   // console.log(feedback[key]);
+
+  //   totalFeedback += feedback[key]
+  // }
+
+  // console.log('totalFeedback: ', totalFeedback);
+
+// const values = Object.values(feedback);
+//  console.log(values);
+
+//  let totalFeedback = 0;
+
+//   for (const value of values) {
+//     console.log(value);
+
+//     totalFeedback +=  value
+//    }
+//    console.log('totalFeedback: ', totalFeedback);
+
+  //  const arr = Object.entries(feedback);
+
+  //  console.log(arr);
--- js/02-friends.js
+++ js/02-friends.js
@@ -1,21 +1,61 @@
 /*
  * Работа с колекцією (масивом об"ектів)
  */
-const friends = [
+// const friends = [
+//     { name: 'Mango', online: false },
+//     { name: 'Kiwi', online: true },
+//     { name: 'Poly', online: false },
+//     { name: 'Ajax', online: false },
+//   ];
+  
+  // console.log(friends[0]);
+  // console.table(friends);
+  
+  // Додаємо властивість до кожного об"єкта за допомогою циклу
+
+  // const nums = [1,2,3,5,4,6]
+
+  // console.log(nums);
+  
+  // for (let num of nums) {
+  //   num += 1
+  // }
+
+  // console.log(nums);
+
+  // for (const friend of friends) {
+  //   console.log(friend);
+    
+  //   friend.name = 123365;
+
+  // }
+  // console.table(friends);
+
+  /*
+   * Шукаємо друга по імені
+   */
+  
+  const friends = [
     { name: 'Mango', online: false },
     { name: 'Kiwi', online: true },
     { name: 'Poly', online: false },
     { name: 'Ajax', online: false },
   ];
-  
+
   console.table(friends);
-  
-  // Додаємо властивість до кожного об"єкта за допомогою циклу
-  /*
-   * Шукаємо друга по імені
-   */
-  
-  const findFriendByName = function () {};
+
+  // const findFriendByName = function (allFriends, nameToFind) {
+  //   for (const friend of allFriends) {
+  //     console.log(friend.name)
+
+  //     if (friend.name === nameToFind) {
+  //       console.log('Урааааа, знайшли!');
+  //       continue;
+  //     } 
+  //       console.log('Упс, не знайшли!');     
+      
+  //   }
+  // };
   
   // console.log(findFriendByName(friends, 'Poly'));
   // console.log(findFriendByName(friends, 'Chelsy'));
@@ -24,26 +64,20 @@ const friends = [
    * Отримуємо імена всіх друзів
    */
   
-  const getAllNames = function () {};
+  const getAllNames = function (allFriends) {
+
+    for (const friend of allFriends) {
+      
+    }
+  };
   
+  console.log(getAllNames(friends));
   
   /*
    * Отримуємо імена тільки тих друзів які онлайн
    */
   const getOnlineFriends = function () {
-    const onlineFriends = [];
-  
-    for (const friend of allFriends) {
-      console.log(friend);
-      console.log(friend.online);
-  
-      if (friend.online) {
-        onlineFriends.push(friend);
-      }
-    }
-  
-    return onlineFriends;
-  };
+  }
   
   // console.log(getOnlineFriends(friends));
   
