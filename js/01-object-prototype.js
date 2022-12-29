225ced0835787b9ee42537bdfcad0e7ed23d6c22
Iryna <irasokolova25@ukr.net>
2022-12-29 20:17:08 +0200

    finish

--- js/01-object-prototype.js
+++ js/01-object-prototype.js
@@ -6,15 +6,36 @@
 */
 // Що таке прототипне наслідування, малюю приклад
 // Як створити прототип для нового об'єкту
-const objA = {
-    a: 5,
-    b: 10,
-}
 
-const objB = {
-    c: 1
-}
 
+// console.log([1, 2, 3, 4]);
+// console.log(['a', 's', 'd']);
+// const objA = {
+//     a: 5,
+//     b: 10,
+// }
+
+const objC = {
+    q: 58,
+    t: 68,
+}
+const objB = Object.create(objC)
+objB.c = 1;
+// console.log(objB);
+
+
+const obj = {};
+// console.log(obj);
+
+const objA = Object.create(objB)
+
+objA.a = 5;
+objA.b = 10;
+objA.q = 23;
+// console.log(objA);
+
+// console.log(objA.jbgfgfv);
+// console.log(objA.hasOwnProperty('c'));
 /**
  * Правило пошуку властивостей в ланцюжку прототипів 
  * Властивість hasOwnProperty
--- js/02-constructors.js
+++ js/02-constructors.js
@@ -8,3 +8,25 @@
  * Властивість prototype
  * Властивість Function.prototype
  */
+
+function Student (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+console.dir(Student.prototype);
+Student.prototype.getAge = function(){
+    this.age +=1;
+}
+
+
+
+const Mango = new Student('Mango', 18)
+
+console.log(Mango);
+
+const Poly = new Student('Poly', 17)
+
+console.log(Poly);
+
+
