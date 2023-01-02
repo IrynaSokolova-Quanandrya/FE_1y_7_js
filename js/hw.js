975d102e85799f26559f31b1ebc76fe2c39ac2ca
Iryna <irasokolova25@ukr.net>
2023-01-02 17:21:57 +0200

    add hw

---
+++ js/hw.js
@@ -0,0 +1,41 @@
+const inventory = {
+    items: ['Knife', 'Gas mask'],
+    add(itemName) {
+      console.log(`Adding ${itemName} to inventory`);
+        console.log(this);
+      this.items.push(itemName);
+    },
+    remove(itemName) {
+      console.log(`Removing ${itemName} from inventory`);
+  console.log(this);
+      this.items = this.items.filter(item => item !== itemName);
+    },
+  };
+
+  const obj = {
+    items: ['Knife', 'Gas mask'],
+    hfh: 'jhgfhg',
+  }
+
+  const obj1 = {
+    items: ['Knife', 'Gas mask'],
+  }
+
+  
+  const invokeInventoryAction = function (itemName, action) {
+    console.log(`Invoking action on ${itemName}`);
+    
+    action(itemName);
+  };
+  
+//   invokeInventoryAction('Medkit', inventory.add.bind(inventory));
+  // Invoking action on Medkit
+  // Adding Medkit to inventory
+  
+//   console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+  
+  invokeInventoryAction('Gas mask', inventory.remove.bind(obj));
+  // Invoking action on Gas mask
+  // Removing Gas mask from inventory
+  
+  console.log(inventory.items); // ['Knife', 'Medkit']
