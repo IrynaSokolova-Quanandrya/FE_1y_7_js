3c6c18393d802b5f47fdd366b7b74b7981c77c88
Iryna <irasokolova25@ukr.net>
2023-01-03 20:55:08 +0200

    finish

--- js/01-class.js
+++ js/01-class.js
@@ -10,6 +10,53 @@
  */
 
 
+// function Car (model, price, year, color){
+//     this.model = model;
+//     this.price = price;
+//     this.year = year;
+//     this.color = color;     
+// }
 
-// const carInstance ;
+// Car.prototype.changePrice = function(newPrice){
+//     this.price = newPrice;
+// }
+
+// Car.description = 'Це функція конструктор!'
+
+// // console.log(Car.model);
+// const carInstance = new Car();
+// const audi = new Car('A5', 40000, 2020, 'black');
+// console.log(audi);
+
+
+// const mazda = new Car('mazda-3', 35000, 2022, 'red');
+// console.log(mazda);
+
+
+class Car {
+    static description = 'Це функція конструктор!'
+
+    constructor(model, price, year, color){
+        this.model = model;
+        this._price = price;
+        this.year = year;
+        this.color = color;
+    }
+
+    get price(){
+        this._price;
+    }
+
+    set price(newPrice){
+        this._price = newPrice;
+    }
+}
+// const audi = new Car('A5', 40000, 2020, 'black');
+// console.log(audi);
+// console.log(audi.price);
+// console.log(audi.price(38000));
+
+
+const mazda = new Car('mazda-3', 35000, 2022, 'red');
+console.log(mazda);
 
