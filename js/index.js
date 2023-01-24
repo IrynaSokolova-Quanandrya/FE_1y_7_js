21f4cc2af5a4a5fcc6b3681e02cf72369b49fc3d
Iryna <irasokolova25@ukr.net>
2023-01-24 11:04:52 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,34 @@
-const numbers = [1, 5, 15, 12, 8, 13]
+// const numbers = [1, 5, 15, 12, 8, 13]
 
-for (const number of numbers) {
-    console.log(number);
-}
+// for (const number of numbers) {
+//     console.log(number);
+// }
+
+const arrs = [
+    { name: "Albert", surname: "Einstein", born: 1879, dead: 1955, id: 1 },
+    { name: "Isaac", surname: "Newton", born: 1643, dead: 1727, id: 2 },
+    { name: "Galileo", surname: "Galilei", born: 1564, dead: 1642, id: 3 },
+    { name: "Marie", surname: "Curie", born: 1867, dead: 1934, id: 4 },
+    { name: "Johannes", surname: "Kepler", born: 1571, dead: 1630, id: 5 },
+    { name: "Nicolaus", surname: "Copernicus", born: 1473, dead: 1543, id: 6 },
+    { name: "Max", surname: "Planck", born: 1858, dead: 1947, id: 7 },
+    { name: "Katherine", surname: "Blodgett", born: 1898, dead: 1979, id: 8 },
+    { name: "Ada", surname: "Lovelace", born: 1815, dead: 1852, id: 9 },
+    { name: "Sarah E.", surname: "Goode", born: 1855, dead: 1905, id: 10 },
+    { name: "Lise", surname: "Meitner", born: 1878, dead: 1968, id: 11 },
+    { name: "Hanna", surname: "Hammarström", born: 1829, dead: 1909, id: 12 }
+    ]
+
+    for (const arr of arrs) {
+        const messages = Object.entries(arr.surname);
+        const MESSAGE = []
+
+        for (const message of messages) {
+            const value = message[1];
+            MESSAGE.push(value)
+          }
+
+          const Message = MESSAGE.includes('C');
+
+          console.log(Message);
+      }
