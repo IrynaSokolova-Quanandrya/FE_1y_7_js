3b6e49a627c6554318585eb9926e52dbbefdc607
Iryna <irasokolova25@ukr.net>
2023-02-22 13:42:43 +0200

    Update 03-racetrack.js

--- src/js/03-racetrack.js
+++ src/js/03-racetrack.js
@@ -108,3 +108,36 @@ run(horses[1]).then(({horse, time}) =>{
 function getRandomTime(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+
+
+
+
+
+//  Math.floor(num) - повертає найбільше ціле число, менше, або дорівнює вказаному
+console.log(Math.floor(1.7)); // 1
+
+// Math.ceil(num) - повертає найменше ціле число,
+// більше, або дорівнює зазначеному числу.
+console.log(Math.ceil(1.2)); // 2
+
+// Math.round(num) - повертає значення числа, округлене до найближчого цілого
+console.log(Math.round(1.2)); // 1
+console.log(Math.round(1.5)); // 2
+
+// Math.max(num1, num2, ...) - повертає найбільше число з набору
+console.log(Math.max(20, 10, 50, 40)); // 50
+
+// Math.min(num1, num2, ...) - повертає найменше число з набору
+console.log(Math.min(20, 10, 50, 40)); // 10
+
+// Math.pow(base, exponent) - зведення в ступінь
+console.log(Math.pow(2, 4)); // 16
+
+// Math.random() - повертає псевдовипадкове число в діапазоні [0, 1)
+console.log(Math.random()); // випадкове число між 0 и 1
+console.log(Math.random() * (10 - 1) + 1); // випадкове число від 1 до 10
+
+
+
+
