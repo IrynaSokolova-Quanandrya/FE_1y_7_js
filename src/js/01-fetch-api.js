72a2adb7210123aeff50d410c20b90ef1e28a5cc
Iryna <irasokolova25@ukr.net>
2023-02-27 23:11:55 +0200

    додає покемона

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -15,12 +15,46 @@
 
 import '../css/common.css';
 import pokemonCardTpl from '../templates/pokemon-card.hbs';
-// import API from './api-service';
-// import getRefs from './get-refs';
+import API from './api-service';
+import getRefs from './get-refs';
 const pokemonCard = document.querySelector('.js-card-container');
 const searchForm = document.querySelector('.js-search-form');
 
-const url = 'https://pokeapi.co/api/v2/pokemon/';
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function makeCard(pokemon) {
+//   return  ` <div class="card">
+//   <div class="card-img-top">
+//     <img src=${pokemon.sprites.front_default}" alt="${pokemon.name}>
+//   </div>
+//   <div class="card-body">
+//     <h2 class="card-title">Ім'я:${pokemon.name} </h2>
+//     <p class="card-text">Вага:${pokemon.weight} </p>
+//     <p class="card-text">Зріст: </p>
+
+//     <p class="card-text"><b>Вміння</b></p>
+//     <ul class="list-group"></ul>
+    
+//       <li class="list-group-item"></li>
+    
+//     </ul>
+//   </div>
+// </div> `
+// }
+
+
+// const url = 'https://pokeapi.co/api/v2/pokemon/';
 
 searchForm.addEventListener('submit', onSearch)
 
@@ -51,40 +85,40 @@ function errorMessage(error) {
   alert(error.message)
 }
 
-// const refs = getRefs();
+const refs = getRefs();
 
-// refs.searchForm.addEventListener('submit', onSearch);
+refs.searchForm.addEventListener('submit', onSearch);
 
-// function onSearch(e) {
-//   e.preventDefault();
+function onSearch(e) {
+  e.preventDefault();
 
-//   const form = e.currentTarget;
-//   const searchQuery = form.elements.query.value;
+  const form = e.currentTarget;
+  const searchQuery = form.elements.query.value;
 
-//   API.fetchPokemon(searchQuery)
-//     .then(renderPokemonCard)
-//     .catch(onFetchError)
-//     .finally(() => form.reset());
-// }
+  API.fetchPokemon(searchQuery)
+    .then(renderPokemonCard)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+}
 
-// function renderPokemonCard(pokemon) {
-//   const markup = pokemonCardTpl(pokemon);
-//   refs.cardContainer.innerHTML = markup;
-// }
+function renderPokemonCard(pokemon) {
+  const markup = pokemonCardTpl(pokemon);
+  refs.cardContainer.innerHTML = markup;
+}
 
-// function onFetchError(error) {
-//   alert('Упс, ми не знайшли вашого покемона!');
-// }
+function onFetchError(error) {
+  alert('Упс, ми не знайшли вашого покемона!');
+}
 
-// // =========================================
+// =========================================
 
-// const url = 'https://newsapi.org/v2/everything?q=cars';
-// const options = {
-//   headers: {
-//     Authorization: '4330ebfabc654a6992c2aa792f3173a3',
-//   },
-// };
+const url = 'https://newsapi.org/v2/everything?q=cars';
+const options = {
+  headers: {
+    Authorization: '4330ebfabc654a6992c2aa792f3173a3',
+  },
+};
 
-// fetch(url, options)
-//   .then(r => r.json())
-//   .then(console.log);
+fetch(url, options)
+  .then(r => r.json())
+  .then(console.log);
