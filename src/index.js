aa6f6e62d3beffc92538d3f8a2563cdee02a1e64
Iryna <irasokolova25@ukr.net>
2023-04-18 20:49:49 +0300

    add get

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import './js/r - get';
+import './js/r - get';
 // import './js/c - post';
 // import './js/u - patch';
 // import './js/d - delete';
--- src/js/r - get.js
+++ src/js/r - get.js
@@ -1,1 +1,3 @@
-
+fetch(' http://localhost:3000/movies')
+.then(response => response.json())
+.then(console.log)
