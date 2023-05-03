8cb7f45af409ebd4e951062baa9080ba9a49057b
Iryna <irasokolova25@ukr.net>
2023-05-03 20:01:23 +0300

    add examples

--- src/js/01-try-catch.js
+++ src/js/01-try-catch.js
@@ -11,3 +11,45 @@
 //    - stack
 //  - Блок catch без об'екту помилки
 
+try {
+    console.log('Код до myName');
+    
+    setTimeout(()=>{
+        try{
+            myName;
+        } catch{
+            console.log('error');
+        }
+        
+    }, 500);
+    
+
+    console.log('Код після myName');
+
+} catch {
+    console.log('Помилка');
+}
+
+console.log('Код за try...catch');
+
+
+
+
+
+
+
+
+
+
+
+// import { error, defaultModules } from '@pnotify/core';
+// import '@pnotify/core/dist/PNotify.css';
+// import * as PNotifyMobile from '@pnotify/mobile';
+// import '@pnotify/mobile/dist/PNotifyMobile.css';
+// import '@pnotify/core/dist/BrightTheme.css';
+// defaultModules.set(PNotifyMobile, {});
+
+
+// error({
+//   text: 'Моя перша нотифікація!',
+// });
--- src/js/02-json-parse.js
+++ src/js/02-json-parse.js
@@ -1,17 +1,9 @@
 const validJSON = '{ "name": "Манго", "age": 3 }';
 const invalidJSON = '{ щось не зрозуміле від бекенду }';
 
-try {
-  console.log(1);
+console.log('До');
 
-  console.log(JSON.parse(invalidJSON));
 
-  console.log(2);
-} catch (error) {
-  console.log(error);
-  if (error.name === 'SyntaxError') {
-    console.log('Помилка парса json ');
-  }
-}
+console.log(JSON.parse(invalidJSON));
 
-console.log('Пфсля try...catch');
+console.log('Після');
