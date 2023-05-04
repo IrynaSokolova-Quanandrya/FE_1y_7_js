331a4e9e425bfb9214cefb20839b2bf582b2aad1
Iryna <irasokolova25@ukr.net>
2023-05-04 13:58:52 +0300

    add material

--- src/js/03-async.js
+++ src/js/03-async.js
@@ -1,6 +1,6 @@
 // Синтаксис async/await
-// Последовательные операции
-// Паралельные операции с Promise.all()
+// Послідовні операції
+// Паралельні операції з Promise.all()
 // try...catch
 
 function getFruit(name) {
@@ -8,31 +8,31 @@ function getFruit(name) {
     strawberry: '🍓',
     kiwi: '🥝 ',
     apple: '🍎',
-  };
-
-  return new Promise((resolve, reject) =>
-    setTimeout(() => resolve(fruits[name]), 500),
-  );
+  };  
 }
 
-async function aMakeSmoothie() {
-  try {
-    console.time('aMakeSmoothie');
-    const apple = getFruit('apple');
-    const kiwi = getFruit('kiwi');
-    const berry = getFruit('strawberry');
 
-    const fruits = await Promise.all([apple, kiwi, berry]);
-    console.log(fruits);
-    console.timeEnd('aMakeSmoothie');
 
-    return fruits;
-  } catch (error) {
-    console.log('Ошибка');
-  }
-}
 
-aMakeSmoothie();
+
+// async function aMakeSmoothie() {
+//   try {
+//     console.time('aMakeSmoothie');
+//     const apple = getFruit('apple');
+//     const kiwi = getFruit('kiwi');
+//     const berry = getFruit('strawberry');
+
+//     const fruits = await Promise.all([apple, kiwi, berry]);
+//     console.log(fruits);
+//     console.timeEnd('aMakeSmoothie');
+
+//     return fruits;
+//   } catch (error) {
+//     console.log('Ошибка');
+//   }
+// }
+
+// aMakeSmoothie();
 
 // async function fn () {}
 
--- src/js/async-crud.js
+++ src/js/async-crud.js
@@ -1,64 +1,64 @@
-const BASE_URL = 'http://localhost:4040';
+const BASE_URL = 'http://localhost:3000';
 
-async function addBook(book) {
+function getMovies() {
+  fetch(`${BASE_URL}/students`)
+  .then(response => response.json())
+  .then(console.log)
+}
+
+function getMovieById(movieId) {
+  fetch(`${BASE_URL}/movies/${movieId}`)
+  .then(response => response.json())
+  .then(console.log)
+}
+
+const newMovie = {
+  title: "NODEJS",
+  director: "Me",
+  genres: [
+    "Drama"
+  ],
+  rating: 10
+}
+function addNewMovie(newMovie) {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(newMovie)
+    }
+const url = `${BASE_URL}/movies`;
+
+   return fetch(url, options)
+    .then(res=>res.json())
+    .then(console.log)
+}
+
+const update = {
+  "title": "Node",
+  "director": "Me"
+}
+function updateMovieById(movieId, update) {
   const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(book),
-  };
+      method: 'PATCH',
+      headers: {
+          'Content-type': 'application/json'
+      },
+      body: JSON.stringify(update)
+  }
+const url = `${BASE_URL}/movies/${movieId}`;
 
-  const response = await fetch(`${BASE_URL}/books`, options);
-  const newBook = await response.json();
-
-  return newBook;
+ return fetch(url, options)
+  .then(res=>res.json())
+  .then(console.log)
 }
 
-// async function addBookAndUpdateUI() {
-//   try {
-//     const book = await addBook({});
-//     console.log(book);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
 
-// addBookAndUpdateUI();
-
-async function fetchBooks() {
-  const response = await fetch(`${BASE_URL}/books`);
-  const books = await response.json();
-  return books;
+function deleteMovieById(movieId){
+  fetch(`${BASE_URL}/movies/${movieId}`, {method: 'DELETE'})
+  .then(res=>res.json())
+  .then(console.log)
 }
 
-async function fetchBookById(bookId) {
-  const response = await fetch(`${BASE_URL}/books/${bookId}`);
-  const book = await response.json();
-  return book;
-}
-
-async function removeBook(bookId) {
-  const url = `${BASE_URL}/books/${bookId}`;
-  const options = {
-    method: 'DELETE',
-  };
-
-  const response = await fetch(url, options);
-  const book = await response.json();
-  return book;
-}
-
-async function updateBookById(update, bookId) {
-  const options = {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(update),
-  };
-
-  const response = await fetch(`${BASE_URL}/books/${bookId}`, options);
-  const book = await response.json();
-  return book;
-}
+deleteMovieById(16)
