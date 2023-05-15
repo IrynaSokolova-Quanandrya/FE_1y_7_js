aa1a222d030cb55c958edc1df91d68732808d697
Iryna <irasokolova25@ukr.net>
2023-05-15 14:06:04 +0300

    some fix

--- src/js/03-colorpicker.js
+++ src/js/03-colorpicker.js
@@ -1,5 +1,5 @@
-// import colorCardTpl from '../templates/color-card.hbs';
-// import colorCardsTpl from '../templates/color-cards.hbs';
+import colorCardTpl from '../templates/color-card.hbs';
+import colorCardsTpl from '../templates/color-cards.hbs';
 import colors from './colors.json';
 import '../css/common.css';
 import '../css/colorpicker.css';
